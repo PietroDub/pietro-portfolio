d424bb642e75c4be836a62d37555042b108eac01
PietroDub <pieti453@gmail.com>
2026-08-20 21:32:10 -0300

    fix: verificando commit.

--- components/Contact/MainTerminal.tsx
+++ components/Contact/MainTerminal.tsx
@@ -117,7 +117,7 @@ export default function Terminal3D() {
 
 
       {/* ===================================== */}
-      {/* BORDA DA TELA */}
+      {/* BORDA DA TELA 1 */}
       {/* ===================================== */}
 
       <Line
